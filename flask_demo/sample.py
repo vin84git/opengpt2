a01f859dbf46b8946b62359d60208093948c502f
Vinoth <vipatcts@gmail.com>
2020-03-18 16:27:18 -0700

    Update sample.py

--- flask_demo/sample.py
+++ flask_demo/sample.py
@@ -48,7 +48,7 @@ def sample_sequence(*, hparams, length, start_token=None, batch_size=None, conte
         context = tf.fill([batch_size, 1], start_token)
 
     def step(hparams, tokens, past=None):
-        lm_output = model.model(hparams=hparams, X=tokens, past=past, reuse=tf.AUTO_REUSE)
+        lm_output = model.model(hparams=hparams, X=tokens, past=past, reuse=tf.compat.v1.AUTO_REUSE)
 
         logits = lm_output['logits'][:, :, :hparams.n_vocab]
         presents = lm_output['present']
